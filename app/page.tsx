dd781dd9157a3f58594793b2c154841cb1eae414
Raquel Betancourt <brsarayb@gmail.com>
2023-06-14 14:26:22 -0300

    Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,16 @@
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="text-center mt-10 mx-auto">
+      <h1 className="head_text text-center">
+        Discover & Share <br className="hidden md:block" />
+        <span className="orange_gradient">AI-Powered Prompts</span>
+      </h1>
+      <p className="desc mx-auto">
+        Promptopia is an open-source AI prompting tool for modern world to
+        discover, create and share creative prompts
+      </p>
+    </div>
+  );
 };
 
 export default Home;
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,21 +1,97 @@
+'use client';
+
 import Link from 'next/link';
 import Image from 'next/image';
+import { useState, useEffect } from 'react';
+import { getProviders } from 'next-auth/react';
 
 const Nav = () => {
-  const isUserLoggedIn = true;
+  const [toggleDropdown, setToggleDropdown] = useState(false);
+  const isUserLoggedIn = false;
   return (
     <>
       <nav className="w-full mt-4 flex gap-3 flex-between">
-        <Link href="/">
-          <Image
-            src="/assets/images/logo.svg"
-            alt="logo"
-            height={30}
-            width={30}
-          />
-        </Link>
-        <p className="logo_text">Promptopia</p>
-        <div>{isUserLoggedIn ? <div>Yes</div> : <div>Not</div>}</div>
+        <div className="flex gap-3">
+          <Link href="/">
+            <Image
+              src="/assets/images/logo.svg"
+              alt="logo"
+              height={30}
+              width={30}
+            />
+          </Link>
+          <p className="logo_text">Promptopia</p>
+        </div>
+
+        {/* Desktop Navigation */}
+
+        <div className="hidden sm:block">
+          {isUserLoggedIn ? (
+            <div className="flex gap-3">
+              <Link href="/create-prompt" className="black_btn">
+                Create Post
+              </Link>
+              <Link href="/create-prompt" className="outline_btn">
+                Create Post
+              </Link>
+              <Link href="/my-profile">
+                <Image
+                  src="/assets/images/logo.svg"
+                  alt="profile image"
+                  height={36}
+                  width={36}
+                />
+              </Link>
+            </div>
+          ) : (
+            <div>
+              <button className="black_btn">Sign in</button>
+            </div>
+          )}
+        </div>
+
+        {/* Mobile Navigation **/}
+        <div className="sm:hidden">
+          {isUserLoggedIn ? (
+            <div className="flex">
+              <Image
+                src="/assets/images/logo.svg"
+                alt="Profile Image"
+                height={36}
+                width={36}
+                className="cursor-pointer"
+                onClick={() => {
+                  setToggleDropdown((prev) => !prev);
+                }}
+              />
+              {toggleDropdown && (
+                <div className="dropdown top-0">
+                  <Link
+                    href="/profile"
+                    className="dropdown_link"
+                    onClick={() => setToggleDropdown(false)}
+                  >
+                    My Profile
+                  </Link>
+                  <Link
+                    href="/create-prompt"
+                    className="dropdown_link"
+                    onClick={() => setToggleDropdown(false)}
+                  >
+                    Create Prompt
+                  </Link>
+                  <button type="button" className="black_btn w-full">
+                    Sign Out
+                  </button>
+                </div>
+              )}
+            </div>
+          ) : (
+            <div>
+              <button className="black_btn">Sign in</button>
+            </div>
+          )}
+        </div>
       </nav>
     </>
   );
