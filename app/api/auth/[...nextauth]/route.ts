522e724eba0a8958f8843d2ed61d04064b2c2992
Raquel Betancourt <brsarayb@gmail.com>
2023-06-24 11:00:35 -0300

    NextAuth config

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,58 @@
 import NextAuth from 'next-auth/next';
 import GoogleProvider from 'next-auth/providers/google';
+import { db } from '@lib/db';
+import { PrismaAdapter } from '@next-auth/prisma-adapter'; // se debe instalar el paquete de manera independiente
+import { nanoid } from 'nanoid';
 
-export default NextAuth({
+const handler = NextAuth({
+  adapter: PrismaAdapter(db), //Esta db es boilerplate code va en la ruta lib/db.ts para que prisma no se instancia varias veces
+  session: {
+    strategy: 'jwt', // ya que se le especifico un adapter la strategy cambia a database, por eso hay que hardcodear la strategy a jwt nuevamente
+  },
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID!,
       clientSecret: process.env.GOOGLE_SECRET!,
     }),
   ],
+  callbacks: {
+    async session({ session, token }) {
+      // que va a pasar cuando una session se cree
+      // crear los types en types/next-auth.d.ts
+      if (token) {
+        session.user.id = token.id;
+        session.user.name = token.name;
+        session.user.email = token.email;
+        session.user.image = token.picture;
+        session.user.username = token.username;
+      }
+      return session;
+    },
+    async jwt({ token, user }) {
+      const dbUser = await db.user.findFirst({
+        where: {
+          email: token.email,
+        },
+      });
+
+      if (!dbUser) {
+        //Cuando el usuario aun no esta en la database
+        token.id = user!.id;
+        return token;
+      }
+
+      if (!dbUser.username) {
+        await db.user.update({
+          where: {
+            id: dbUser.id,
+          },
+          data: {
+            username: nanoid(10),
+          },
+        });
+      }
+    },
+  },
 });
+
+export { handler as GET, handler as POST };
