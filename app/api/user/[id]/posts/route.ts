45ae7b890d9e41c49bce42f5c765a17bda3fe9fc
Raquel Betancourt <brsarayb@gmail.com>
2023-07-27 07:12:19 -0300

    route.ts

--- app/api/user/[id]/posts/route.ts
+++ app/api/user/[id]/posts/route.ts
@@ -1,10 +1,9 @@
 import { PrismaClient } from '@prisma/client';
 import { NextResponse } from 'next/server';
-import type { NextApiRequest } from 'next';
 
 const prisma = new PrismaClient();
 
-export async function GET(req: NextApiRequest, { params }: any) {
+export async function GET(req: Request, { params }: any) {
   const { id } = params;
 
   const promptsByUser = await prisma.user.findMany({
